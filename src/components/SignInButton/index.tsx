82bbc0ea9fc0c79993e41f01d38fe497d06b25b1
fakebmarchete <leonardo.ngra@gmail.com>
2022-09-05 10:31:30 -0300

    API Routes no Next.js

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,19 +1,22 @@
+import { signIn, signOut, useSession } from 'next-auth/react';
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+
 
 import styles from './styles.module.scss';
 
 export function SignInButton(){
 
-    const isUserLoggedIn = true;
+    const {data: session} = useSession()
 
-    return isUserLoggedIn ? (
+    return session ? (
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signOut()}
         >
             <FaGithub color='#04d361' />
-            Leonardo Nogueira
+            {session.user.name}
             <FiX color='737380' className={styles.closeIcon}/>
         </button>
 
@@ -21,6 +24,7 @@ export function SignInButton(){
         <button 
             type="button"
             className={styles.signInButton}
+            onClick={() => signIn('github')}
         >
           <FaGithub color='#eba417' />
           Sing in with GitHub
