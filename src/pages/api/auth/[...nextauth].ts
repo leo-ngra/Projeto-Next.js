7fb9e15e84245178950077d7b31a1a7011678a7c
fakebmarchete <leonardo.ngra@gmail.com>
2022-09-05 12:13:31 -0300

    inserindo faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q} from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProviders from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   
@@ -14,4 +18,41 @@ export default NextAuth({
           }
         }),
       ],
+
+      callbacks: {
+        async signIn({ user, account, profile }) {
+          const {email} = user
+          
+          try{
+            await fauna.query(
+             q.If(
+              q.Not(
+                q.Exists(
+                  q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(user.email)
+                  )
+                )
+              ),
+              q.Create(
+                q.Collection('users'),
+                {data: {email}}
+              ),
+              q.Get(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+               )
+              )
+             )
+            )
+            return true
+          } catch {
+            return false
+          }
+         
+          
+          return true
+        },
+      }
 })
